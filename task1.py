163b4255c5f49ac93bf4840222e370ff2a967e9a
ccallow <71406811+ccallow@users.noreply.github.com>
2020-11-02 11:45:46 -0800

    2
    22

--- task1.py
+++ task1.py
@@ -6,3 +6,22 @@ the completed madlib.
 What is a madlib? Visit https://www.madlibs.com/printables/ to see some Madlibs
 you might use in your assignment
 """
+
+import tkinter as tk 
+from tkinter import *
+
+win = tk.Tk()
+#text code
+entry1 = tk.Entry(win, textVariable = labelName)
+
+#text variables
+labelName = StringVar()
+labelName.set("Name")
+labelVerb = StringVar()
+labelVerb.set("Verb")
+labelAdj = StringVar()
+labelAdj.set("Adjective")
+labelAnimal = StringVar()
+labelAnimal.set("Animal")
+
+win.mainloop
