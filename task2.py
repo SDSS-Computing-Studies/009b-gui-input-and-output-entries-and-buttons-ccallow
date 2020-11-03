c5bc672d311e1c9b7af7961de1db7031dbc4f729
ccallow <71406811+ccallow@users.noreply.github.com>
2020-11-03 13:01:13 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -14,41 +14,93 @@ between + and -.
 """
 import tkinter as tk 
 from tkinter import *
+import math
 
 win = tk.Tk()
 win.title("Factoring Trinomials")
 
 #textvaribles
 binput = StringVar()
-binput.set("")
+binput.set("b")
 cinput = StringVar()
-cinput.set("")
+cinput.set("c")
+eoutput = StringVar()
+eoutput.set("Answers go here!")
 
 #instructions
-inst1 = Label (win, text = "Today we are going to factor trinomials. \n 1. Enter two values for the b-coefficient and c-coefficient. \n 2. Hit the factor button and the answer will appear. \n *** If you want to make the b or c values negative, remember a negative sign.***", anchor = W, justify = LEFT)
-inst2  = Label (win, text  = "1. Enter two values for the b-coefficient and c-coefficient.")
-inst3 = Label (win, text = "2. Hit the factor button and the answer will appear.")
-instNote = Label(win, text  = "*** If you want to make the b or c values negative, remember a negative sign.***" )
+inst1 = Label (win, text = "Today we are going to factor trinomials. \n 1. Enter two values for the b-coefficient and c-coefficient. \n 2. Hit the factor button and the answer will appear. \n *** If you want to make the b or c values negative, select it in the dropmenu.*** \n      ", anchor = W, justify = LEFT)
 entry = Label(win, text = "Enter your values:")
 
 #entry boxes
-a = Label (win, text = "x^2 +")
-bentry = Entry (win, textvariable = binput, width = 5)
+a = Label (win, text = "x^2 ")
+bentry = Entry (win, textvariable = binput, width = 3)
 b = Label (win, text = "x")
-centry = Label (win, textvariable = cinput, width = 5)
+centry = Entry (win, textvariable = cinput, width = 3)
+
+#dropdown boxes
+option1 = StringVar(win)
+option1.set("+")
+option2 = StringVar(win)
+option2.set("+")
+w = OptionMenu(win, option1, "+", "-")
+w2 = OptionMenu(win, option2, "+", "-")
+
+def quadratic():
+    b = bentry.get()
+    b = int(b)
+    c = centry.get()
+    c = int(c)
+    bsign = option1.get()
+    csign = option2.get()
+    if bsign == "-" and csign == "+":
+        b = b*-1
+        d = (b**2) - (4*c)
+        answer1 = (-b-math.sqrt(d))/(2)
+        answer2 = (-b+math.sqrt(d))/(2)
+    elif bsign == "-" and csign == "-":
+        c = c*(-1)
+        b = b*(-1)
+        d = (b**2) - (4*c)
+        answer1 = (-b-math.sqrt(d))/(2)
+        answer2 = (-b+math.sqrt(d))/(2)
+    elif bsign == "+" and csign == "-":
+        c = c*(-1)
+        d = (b**2) - (4*c)
+        answer1 = (-b-math.sqrt(d))/(2)
+        answer2 = (-b+math.sqrt(d))/(2)
+    else:
+        d = (b**2) - (4*c)
+        answer1 = (-b-math.sqrt(d))/(2)
+        answer2 = (-b+math.sqrt(d))/(2)
+    answer1 = round(answer1, 5)
+    answer2 = round(answer2, 5)
+    answer1 = str(answer1)
+    answer2 = str(answer2)
+    line = "The values of x:  " + answer1 +" and " + answer2
+    a_entry.delete(0,END)
+    a_entry.insert(0, line)
+
+#bottomcode
+bottomcode = Label(win, text = "\n ")
+button1 = Button(win, text = "The value of x: ", command = quadratic)
+a_entry = Entry(win, width = 40, textvariable=eoutput)
 
 #grid for instructions
 inst1.grid(row = 1, column = 1, columnspan = 15)
-#inst2.grid(row = 2, column = 1, columnspan = 6, sticky = W)
-#inst3.grid(row = 3, column = 1, columnspan = 5, sticky = W)
-#instNote.grid(row = 4, column = 1, columnspan= 8, sticky = W)
-entry.grid( row = 5, column = 1, columnspan = 2, sticky = W)
+entry.grid( row = 6, column = 1, columnspan = 2, sticky = W)
 
 #grid for code
-a.grid(row = 6, column = 1, sticky = E)
-bentry.grid(row = 6, column = 2, sticky = W)
-b.grid(row = 6, column = 3, sticky = E)
-centry.grid (row = 6, column = 4, sticky = W)
+a.grid(row = 7, column = 1, sticky = E)
+w.grid(row = 7, column = 2)
+bentry.grid(row = 7, column = 3, sticky = E)
+b.grid(row = 7, column = 4, sticky = W)
+w2.grid(row = 7, column = 5)
+centry.grid (row = 7, column = 6, sticky = E)
+
+#bottom code
+bottomcode.grid (row = 8, column = 1)
+button1.grid(row = 9, column = 1)
+a_entry.grid(row = 10, column = 1, columnspan = 4, sticky = W)
 
 
 win.mainloop()
